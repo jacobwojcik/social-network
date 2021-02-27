9e250bb48f38369ed3bb8d6ae4acc2358e080670
Jac00bv <jacobyv11@gmail.com>
2021-02-27 20:00:46 +0100

    readme files

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
-
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(public _auth: AuthService) {}
+  public href: string = '';
+  constructor(public _auth: AuthService, private router: Router) {
+    router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.href = this.router.url;
+      });
+  }
 
-  ngOnInit(): void {}
+  ngOnInit() {}
 }
